f50077ba93b94d8aab5da25772add54df05e6f38
ck <jut@local.lab>
2025-11-26 00:11:41 +0100

    TF Project build 1

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  type        = string
+  description = "GCP Project name"
+  default     = "ocp-on-gcp-476018"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP Region name"
+  default     = "europe-west9"
+}
+
+variable "zone" {
+  type        = string
+  description = "GCP Zone name"
+  default     = "europe-west9-&"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "GCP VPC Name"
+  default     = "terraform-network"
+}
